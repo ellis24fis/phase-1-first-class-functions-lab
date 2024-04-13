18f24b84a95887043962cce6afdf8bea070956f1
Valerie Ellis <valerie.ellis@flatironschool.com>
2024-04-13 16:50:44 -0400

    submit assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+function returnFirstTwoDrivers(drivers){
+    const twoDrivers = drivers.slice(0,2)
+    return twoDrivers
+}
+
+function returnLastTwoDrivers(drivers){
+    const lastTwoDrivers = drivers.slice(-2)
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ 
+function createFareMultiplier(multiplier){
+return ((fare) => {return fare * multiplier});
+}
+
+function fareDoubler(fare){
+return fare*2
+}
+
+function fareTripler(fare){
+    return fare*3
+    }
+
+function selectDifferentDrivers(arrayOfDrivers, functionThe){    
+    return functionThe(arrayOfDrivers);
+}
